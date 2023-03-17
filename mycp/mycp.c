18e0c1a97c16b20423c08d55e88aa49682310f37
Yummi <jqhinno@gmail.com>
2023-03-17 10:49:43 +0900

    Update mycp.c

--- mycp/mycp.c
+++ mycp/mycp.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[]){
 	
 	fd_orgin = open(argv[1], O_RDONLY);
 	//fd_origin = /*	TODO    [1] fill out here using system call  */;
-	fd_dest = open(argv[2], O_RDWR|O_CREAT, 0664);
+	fd_dest = open(argv[2], O_RDWR|O_CREAT|O_SYNC, 0664);
 	//fd_dest = /*    TODO	[1-1] fill out here using system call*/;
 
 	if (fd_origin < 0 || fd_dest <0){
@@ -26,9 +26,9 @@ int main(int argc, char *argv[]){
 	}
 
 	//read from the origin file
-	while((read_size = read(fd_orgin, buf, sizeof(MAX_BUF)))!= 0){
+	while((read_size = read(fd_orgin, buf, MAX_BUF))!= 0){
         //write to the dest file
-		write_size = write(fd_dest, buf, sizeof(MAX_BUF));
+		write_size = write(fd_dest, buf, read_size);
 	}
 	close(fd_dest);
     close(fd_origin);
